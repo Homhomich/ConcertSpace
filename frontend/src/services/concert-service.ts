29effd498745ce21aba27a88de5246e463148643
anna_yankevich <anna_yankevich@relex.ru>
2021-06-13 11:31:56 +0300

    Fix urls for deploy

--- frontend/src/services/concert-service.ts
+++ frontend/src/services/concert-service.ts
@@ -4,16 +4,16 @@ import {UserModel} from '../models/user-model';
 import {TicketModel} from '../models/ticket-model';
 
 export async function getAllConcerts(): Promise<ConcertModel[]> {
-	return await axios.get('http://localhost:8080/concerts/all').then((response) => response.data);
+	return await axios.get('https://concertspace-backend.herokuapp.com/concerts/all').then((response) => response.data);
 }
 
 export async function getSearchedConcerts(search: string): Promise<ConcertModel[]> {
-	return await axios.get(`http://localhost:8080/concerts/search?search=${search}`, {params: search}).then((response) => response.data);
+	return await axios.get(`https://concertspace-backend.herokuapp.com/concerts/search?search=${search}`, {params: search}).then((response) => response.data);
 }
 
 export async function putNewBoughtTicket(ticketId: number, user: Partial<UserModel>, concertId?: number): Promise<TicketModel> {
 	console.log(JSON.stringify(user));
-	return await axios.post('http://localhost:8080/concerts/buy', user, {
+	return await axios.post('https://concertspace-backend.herokuapp.com/concerts/buy', user, {
 		params: {
 			concertId: concertId,
 			ticketSettingsId: ticketId
--- frontend/src/services/venue-service.ts
+++ frontend/src/services/venue-service.ts
@@ -1,20 +1,19 @@
 import axios from 'axios';
 import {VenueRentModel} from '../models/venue-rent-model';
 import {VenueModel} from '../models/venue-model';
-import {venues} from './test-values';
 
 export async function getAllVenues(): Promise<VenueModel[]> {
-	return await axios.get('http://localhost:8080/venues/all').then((response) => response.data);
+	return await axios.get('https://concertspace-backend.herokuapp.com/venues/all').then((response) => response.data);
 }
 
 export async function getSearchedVenues(search: string): Promise<VenueModel[]> {
-	return await axios.get(`http://localhost:8080/venues/search?search=${search}`)
+	return await axios.get(`https://concertspace-backend.herokuapp.com/venues/search?search=${search}`)
 		.then((response) => response.data);
 }
 
 export async function putRentedVenue(venueId: number, venueRent: VenueRentModel): Promise<VenueModel> {
 	console.log(JSON.stringify(venueRent));
-	return await axios.post('http://localhost:8080/venues/venue/rent', venueRent, {
+	return await axios.post('https://concertspace-backend.herokuapp.com/venues/venue/rent', venueRent, {
 		params: {venueId: venueId}
 	}).then((response) => response.data);
 }
